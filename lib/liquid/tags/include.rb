58502bc19c034323ec252e01c6d29f324c8f9fa3
fastjames <fastjames@gmail.com>
2017-11-02 13:03:21 -0500

    Make 'include with' different from 'include for'
    The include tag currently accepts the 'with' and 'for' qualifiers when a
    local variable is passed in. The documentation at
    http://www.rubydoc.info/github/Shopify/liquid/Liquid/Include
    implies that these different terms cause different behavior, but this
    has never been the case. Consequently, when a variable exists in the
    parent template with the same name as the partial it is not possible to
    pass an array to that liquid partial without the partial being rendered
    once for each element in the same-named array.
    
    This change allows the caller to pass an array in using the 'with'
    keyword and force the partial to be rendered once. The default behavior
    for same-named variables, and the behavior with the for keyword, remain
    unchanged.

--- lib/liquid/tags/include.rb
+++ lib/liquid/tags/include.rb
@@ -14,7 +14,7 @@ module Liquid
   #   {% include 'product' for products %}
   #
   class Include < Tag
-    Syntax = /(#{QuotedFragment}+)(\s+(?:with|for)\s+(#{QuotedFragment}+))?/o
+    Syntax = /(#{QuotedFragment}+)(\s+(with|for)\s+(#{QuotedFragment}+))?/o
 
     def initialize(tag_name, markup, options)
       super
@@ -22,11 +22,13 @@ module Liquid
       if markup =~ Syntax
 
         template_name = $1
-        variable_name = $3
+        local_inclusion_type = $3
+        variable_name = $4
 
         @variable_name_expr = variable_name ? Expression.parse(variable_name) : nil
         @template_name_expr = Expression.parse(template_name)
         @attributes = {}
+        @local_inclusion_type = local_inclusion_type
 
         markup.scan(TagAttributes) do |key, value|
           @attributes[key] = Expression.parse(value)
@@ -63,7 +65,7 @@ module Liquid
             context[key] = context.evaluate(value)
           end
 
-          if variable.is_a?(Array)
+          if variable.is_a?(Array) && @local_inclusion_type != 'with'
             variable.collect do |var|
               context[context_variable_name] = var
               partial.render(context)
